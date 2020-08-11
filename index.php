e31ce0394abdd27d0b9326464f0dc445b92b8246
alexangria1992 <alex040892@gmail.com>
2020-08-12 09:44:32 +1200

    Added more catgegory_nav styling

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
     <link href="styles/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles/style.css"> 
+    <link rel="stylesheet" href="styles/category_nav_style.css"> 
     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 </head>
 <body>
