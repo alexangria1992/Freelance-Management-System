9e90be3d77f7ffca21990bdaf1f32002a6c8435c
alexangria1992 <alex040892@gmail.com>
2020-08-12 00:07:25 +1200

    added navigation links and search bar

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,38 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <a href="./Freelance-Management-System/index.php" class="navbar-brand">Computerfever</a>
-        <a href="./Freelance-Management-System/mobile_categories.php/" class="navbar-toggler"><i class="fa fa-th-large"></i></a>
+        <a href="./Freelance-Management-System/mobile_categories.php/" class="navbar-toggler"><i class="fa fa-th-large"
+        style = "font-size:1.5em;"></i></a>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <hr>
+
+            <form action="" method="post" class="form-inline mr-auto">
+                <div class="input-group">
+                    <input type="text" class="form-control" required placeholder="Search Proposals" name="search_query">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" name="search" type="submit">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </form>
+            <hr>
+
+            <ul class="navbar-nav">
+                <li class="nav-item-active">
+                    <a href="./index.php" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link" data-target="#register-modal">Become a Seller</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link" data-target="#login-modal">Sign In</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="btn btn-primary btn-sm" data-target="#register-modal">Sign Up</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
