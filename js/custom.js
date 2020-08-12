8f540f102b71826e91513982bdd734e25f6f7c06
alexangria1992 <alex040892@gmail.com>
2020-08-12 19:47:05 +1200

    Added jquery owl carousel tootltip function

--- js/custom.js
+++ js/custom.js
@@ -16,5 +16,44 @@ $(document).ready(function(){
 
     $(window).scroll(function(){
         stickyNav();
-    })
+    });
+
+    $(".home-featured-carousel").owlCarousel({
+        items: 4,
+        margin: 30,
+        stagePadding: 30,
+        autoplay:true,
+        autoplaySpeed:1000,
+        responsiveClass:true,
+        responsive: {
+            0:{
+                items:1,
+            },
+
+            480:{
+                items:1,
+            },
+
+            600:{
+                items:2
+            },
+
+            768:{
+                items:3
+            },
+
+            1000:{
+                items: 4
+            }
+        }
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+
+
+
+
+
+
 });
