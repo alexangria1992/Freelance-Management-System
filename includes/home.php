47291e0171f3c9723c7de037e2d6c66039146fc5
alexangria1992 <alex040892@gmail.com>
2020-08-12 20:57:24 +1200

    Styled the ribbon and fixed a mistake on strong class

--- includes/home.php
+++ includes/home.php
@@ -161,7 +161,51 @@
                     <div class="proposal-div">
                         <div class="proposal_nav">
                             <span class="float-left mt-2">
-                                <strong class="float-left mt-2">
+                                <strong class="ml-2 mr-1">
+                                By
+                                </strong>
+                                fixmywebsite
+                            </span>
+                            <span class="float-right mt-2">
+                                <img src="images/user_rate_full.png" alt="" class="rating">
+                                <img src="images/user_rate_full.png" alt="" class="rating">
+                                <img src="images/user_rate_full.png" alt="" class="rating">
+                                <img src="images/user_rate_full.png" alt="" class="rating">
+                                <img src="images/user_rate_full.png" alt="" class="rating">
+                                <img src="images/user_rate_full.png" alt="" class="rating">
+                                <span class="ml-1 mr-2">(4)</span>
+                            </span>
+                            <div class="clearfix mb-2">
+
+                            </div>
+                        </div>
+                        <a href="proposals/proposal.php">
+                            <hr class="m-0 p-0">
+                            <img src="proposals/proposal_files/youtube-seo-1.jpg" class="resp-img" alt="">
+                        </a>
+                        <div class="text">
+                            <h4>
+                                <a href="proposals/proposal.php" class="video-img">
+                                    I will Do Viral Youtube Seo Social Media Promotion
+                                </a>
+                            </h4>
+                            <hr>
+                            <p class="buttons clearfix">
+                                <a href="" class="favorite mt-2 float-left" data-toggle="tooltip" title="Add To Favorites">
+                                    <i class="fa fa-heart fa-lg"></i>
+                                </a>
+                                <span class="float-right">STARTING AT <strong class="price">$5</strong></span>
+                            </p>
+                        </div>
+                        <div class="ribbon">
+                            <div class="theribbon"> Featured </div>
+                            <div class="ribbon-background"></div>
+                        </div>
+                    </div>
+                    <div class="proposal-div">
+                        <div class="proposal_nav">
+                            <span class="float-left mt-2">
+                                <strong class="ml-2 mr-1">
                                     By
                                 </strong>
                                 fixmywebsite
@@ -205,7 +249,7 @@
                     <div class="proposal-div">
                         <div class="proposal_nav">
                             <span class="float-left mt-2">
-                                <strong class="float-left mt-2">
+                                <strong class="ml-2 mr-1">
                                     By
                                 </strong>
                                 fixmywebsite
@@ -249,7 +293,7 @@
                     <div class="proposal-div">
                         <div class="proposal_nav">
                             <span class="float-left mt-2">
-                                <strong class="float-left mt-2">
+                                <strong class="ml-2 mr-1">
                                     By
                                 </strong>
                                 fixmywebsite
@@ -293,7 +337,7 @@
                     <div class="proposal-div">
                         <div class="proposal_nav">
                             <span class="float-left mt-2">
-                                <strong class="float-left mt-2">
+                                <strong class="ml-2 mr-1">
                                     By
                                 </strong>
                                 fixmywebsite
@@ -337,7 +381,7 @@
                     <div class="proposal-div">
                         <div class="proposal_nav">
                             <span class="float-left mt-2">
-                                <strong class="float-left mt-2">
+                                <strong class="ml-2 mr-1">
                                     By
                                 </strong>
                                 fixmywebsite
@@ -381,7 +425,7 @@
                     <div class="proposal-div">
                         <div class="proposal_nav">
                             <span class="float-left mt-2">
-                                <strong class="float-left mt-2">
+                                <strong class="ml-2 mr-1">
                                     By
                                 </strong>
                                 fixmywebsite
@@ -425,51 +469,7 @@
                     <div class="proposal-div">
                         <div class="proposal_nav">
                             <span class="float-left mt-2">
-                                <strong class="float-left mt-2">
-                                    By
-                                </strong>
-                                fixmywebsite
-                            </span>
-                            <span class="float-right mt-2">
-                                <img src="images/user_rate_full.png" alt="" class="rating">
-                                <img src="images/user_rate_full.png" alt="" class="rating">
-                                <img src="images/user_rate_full.png" alt="" class="rating">
-                                <img src="images/user_rate_full.png" alt="" class="rating">
-                                <img src="images/user_rate_full.png" alt="" class="rating">
-                                <img src="images/user_rate_full.png" alt="" class="rating">
-                                <span class="ml-1 mr-2">(4)</span>
-                            </span>
-                            <div class="clearfix mb-2">
-
-                            </div>
-                        </div>
-                        <a href="proposals/proposal.php">
-                            <hr class="m-0 p-0">
-                            <img src="proposals/proposal_files/youtube-seo-1.jpg" class="resp-img" alt="">
-                        </a>
-                        <div class="text">
-                            <h4>
-                                <a href="proposals/proposal.php" class="video-img">
-                                    I will Do Viral Youtube Seo Social Media Promotion
-                                </a>
-                            </h4>
-                            <hr>
-                            <p class="buttons clearfix">
-                                <a href="" class="favorite mt-2 float-left" data-toggle="tooltip" title="Add To Favorites">
-                                    <i class="fa fa-heart fa-lg"></i>
-                                </a>
-                                <span class="float-right">STARTING AT <strong class="price">$5</strong></span>
-                            </p>
-                        </div>
-                        <div class="ribbon">
-                            <div class="theribbon">Featured</div>
-                            <div class="ribbon-background"></div>
-                        </div>
-                    </div>
-                    <div class="proposal-div">
-                        <div class="proposal_nav">
-                            <span class="float-left mt-2">
-                                <strong class="float-left mt-2">
+                                <strong class="ml-2 mr-1">
                                     By
                                 </strong>
                                 fixmywebsite
