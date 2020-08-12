e685b7f7b5d687ddab59a08daa19d72128c23948
alexangria1992 <alex040892@gmail.com>
2020-08-12 16:03:10 +1200

    Created how it works header modal

--- includes/home.php
+++ includes/home.php
@@ -25,3 +25,33 @@
         </center>
     </div>
 </header>
+
+<div class="modal fade" id="how-it-work">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">How Computerfever works</h5>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span>&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <h4 align="center">How To Sell</h4>
+                <p>Create a new job for a service you can offer.</p>
+                <p>Share your jb using our social bookmarking tools.</p>
+                <p>You will be notified when someone orders your job.</p>
+                <p>After you deliver your work we will credit your account with $4</p>
+                <p>Withdraw your earnings yo your Paypal account</p>
+                <p>Spend your money!</p>
+                <h4 align="center">How To buy</h4>
+                <p>Find a job you like and place and order</p>
+                <p>Pay for your job using Paypal or a credit card.</p>
+                <p>Track the seller's progress, communicate and exchange files.</p>
+                <p>Receive your finished work.</p>
+                <p>You can request fixes from the seller if it is not what you wanted</p>
+                <p>Provide feedback on the seller and the job</p>
+
+            </div>
+        </div>
+    </div>
+</div>
