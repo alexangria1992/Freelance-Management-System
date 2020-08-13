2358f9915fb1bdd4546589317ba74057e54193d5
alexangria1992 <alex040892@gmail.com>
2020-08-13 14:54:10 +1200

    mobile categories page created

---
+++ mobile_categories.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Computer-Fever Freelance Services Marketplace</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <link href="styles/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/style.css"> 
+    <link rel="stylesheet" href="styles/category_nav_style.css"> 
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link href="styles/owl.carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/owl.theme.default.css">
+
+</head>
+<body>
+    <?php include("includes/header.php"); ?>
+     <div class="container mt-5">
+         <h2 class="text-center mb-4">Computerfever Categories</h2>
+         <div class="text-center mb-4">
+             <div class="mobile-category">
+                 <a href="category.php?cat_id">
+                     <div class="ml-2 mt-3 category-picture">
+                         <img src="cat_images/graphic-design.png" alt="">
+                     </div>
+                     <div class="category-text">
+                         <p class="category-title">
+                             <strong>Graphic Designing</strong>
+                         </p>
+                         <p class="mb-4 category-desc">
+                             It is a long established fact that a reader will be distract
+                         </p>
+                     </div>
+                 </a>
+             </div>
+
+             <div class="mobile-category">
+                 <a href="category.php?cat_id">
+                     <div class="ml-2 mt-3 category-picture">
+                         <img src="cat_images/graphic-design.png" alt="">
+                     </div>
+                     <div class="category-text">
+                         <p class="category-title">
+                             <strong>Graphic Designing</strong>
+                         </p>
+                         <p class="mb-4 category-desc">
+                             It is a long established fact that a reader will be distract
+                         </p>
+                     </div>
+                 </a>
+             </div>
+
+             <div class="mobile-category">
+                 <a href="category.php?cat_id">
+                     <div class="ml-2 mt-3 category-picture">
+                         <img src="cat_images/graphic-design.png" alt="">
+                     </div>
+                     <div class="category-text">
+                         <p class="category-title">
+                             <strong>Graphic Designing</strong>
+                         </p>
+                         <p class="mb-4 category-desc">
+                             It is a long established fact that a reader will be distract
+                         </p>
+                     </div>
+                 </a>
+             </div>
+
+             <div class="mobile-category">
+                 <a href="category.php?cat_id">
+                     <div class="ml-2 mt-3 category-picture">
+                         <img src="cat_images/graphic-design.png" alt="">
+                     </div>
+                     <div class="category-text">
+                         <p class="category-title">
+                             <strong>Graphic Designing</strong>
+                         </p>
+                         <p class="mb-4 category-desc">
+                             It is a long established fact that a reader will be distract
+                         </p>
+                     </div>
+                 </a>
+             </div>
+         </div>
+     </div>
+    <?php include("includes/footer.php"); ?>
+
+
+
+ 
+</body>
+</html>
