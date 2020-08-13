bad71c64b8f60b044529db966a7dc0c668718556
alexangria1992 <alex040892@gmail.com>
2020-08-13 14:09:02 +1200

    prepared search-proposals page

--- search.php
+++ search.php
@@ -29,6 +29,40 @@
             <div class="col-lg-3 col-md-4 col-sm-12">
                 <?php include("includes/search_sidebar.php"); ?>
             </div>
+            <div class="col-lg-9 col-md-8 col-sm-12">
+                <div class="row flex-wrap" id="search-proposals">
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                        <div class="proposal-div">
+                            <div class="proposal_nav">
+                                <span class="float-left">
+                                    <strong class="ml-2 mr-1">
+                                        By
+                                    </strong>
+                                    fixmywebsite
+                                </span>
+                                <span class="float-right mt-2">
+                                    <img src="images/user_rate_full.png" alt="" class="rating">
+                                    <img src="images/user_rate_full.png" alt="" class="rating">
+                                    <img src="images/user_rate_full.png" alt="" class="rating">
+                                    <img src="images/user_rate_full.png" alt="" class="rating">
+                                    <img src="images/user_rate_full.png" alt="" class="rating">
+                                    <span class="ml-1 mr-2">(4)</span>
+                                </span>
+                                <div class="clearfix mb-2">
+
+                                </div>
+                            </div>
+                            <a href="proposals/proposal.php">
+                            <hr class="m-0 p-0">
+                            <img src="proposals/proposal_files/youtube-seo-1.jpg" alt="" class="resp-img">
+                            </a>
+                            <div class="text">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <?php include("includes/footer.php"); ?>
