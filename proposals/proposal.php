06ed4f16d882d2fc65bd696a712f144305abbe2e
alexangria1992 <alex040892@gmail.com>
2020-08-13 21:23:31 +1200

    changed the href linking in  proposal and copied from search into category

---
+++ proposals/proposal.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Hey, thanks for checking my proposal. if you are finding an effective video promotion
+    service then you are in the right place. How does it work? I have some spec">
+    <meta name="author" content="miss_digimarket">
+    <title>I Will Do Viral Youtube SEO Social Media Promotion</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <link href="../styles/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../styles/style.css"> 
+    <link rel="stylesheet" href="../styles/category_nav_style.css"> 
+    <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css">
+    <link href="../styles/owl.carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="../styles/owl.theme.default.css">
+
+</head>
+<body>
+    <?php include("../includes/header.php"); ?>
+
+    <?php include("../includes/footer.php"); ?>
+
+
+
+ 
+</body>
+</html>
