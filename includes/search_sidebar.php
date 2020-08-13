143291e54021bb314cd101690131af3bb9c98df4
alexangria1992 <alex040892@gmail.com>
2020-08-13 12:36:29 +1200

    Added Categories card in sidenav

---
+++ includes/search_sidebar.php
@@ -0,0 +1,46 @@
+<div class="card border-primary mb-3">
+   <div class="card-body pb-2 pt-3">
+       <ul class="nav flex-column">
+           <li class="nav-item checkbox checkbox-primary">
+               <label>
+                   <input type="checkbox" value="1" class="get_online_sellers">
+                   <span>Show Online Sellers</span>
+               </label>
+           </li>
+
+       </ul>
+   </div> 
+</div>
+
+<div class="card border-primary mb-3">
+
+<div class="card-header bg-primary">
+    <h3 class="float-left text-white h5">
+        Categories 
+    </h3>
+    <button class="btn btn-secondary btn-sm float-right clear_cat_id clearlink" onclick="clearCat()">
+        <i class="fa fa-times-circle"></i> Clear Filter
+   </button>
+</div>
+
+<div class="card-body">
+    <ul class="nav flex-column">
+    <li class="nav-item checkbox checkbox-primary">
+        <input type="checkbox" value="1" class="get_cat_id">
+        <span>Graphic & Design</span>
+    </li>
+    <li class="nav-item checkbox checkbox-primary">
+        <input type="checkbox" value="1" class="get_cat_id">
+        <span>Graphic & Design</span>
+    </li>
+    <li class="nav-item checkbox checkbox-primary">
+        <input type="checkbox" value="1" class="get_cat_id">
+        <span>Graphic & Design</span>
+    </li>
+    <li class="nav-item checkbox checkbox-primary">
+        <input type="checkbox" value="1" class="get_cat_id">
+        <span>Graphic & Design</span>
+    </li>
+    </ul>
+</div>
+</div>
