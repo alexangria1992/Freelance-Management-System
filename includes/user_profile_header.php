f0fe310d7e9755a995dca6d2322734dcb48f96f5
alexangria1992 <alex040892@gmail.com>
2020-08-14 22:06:38 +1200

    created user.php and included user_header.php

---
+++ includes/user_profile_header.php
@@ -0,0 +1,24 @@
+<div class="col-md-12 user-header pl-5 pr-5 pt-5 pb-5">
+    <div class="profile-image float-lg-left float-md-left float-none mr-4">
+        <img src="user_images/miss_digimarket.jpg" class="rounded-circle" alt="">
+        <img src="images/level_badge_1.png" class="level_badge" alt="">
+    </div>
+    <div class="content-bar mt-3">
+        <h1>Hi, I'm miss_digimarket</h1>
+        <span class="headline">
+            We have a professional team comprising on different expertise.
+
+        </span>
+        <div class="star-rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star-o"></i>
+            <span class="text-white m-1"><strong>4.0</strong>(3)</span>
+            <span class="text-white">
+                <i class="fa fa-map-marker m-1"></i> Pakistan
+            </span>
+        </div>
+    </div>
+</div>
---
+++ user.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Computer-Fever Freelance Services Marketplace</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <link href="styles/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/style.css"> 
+    <link rel="stylesheet" href="styles/category_nav_style.css"> 
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link href="styles/owl.carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/owl.theme.default.css">
+
+</head>
+<body>
+    <?php include("includes/header.php"); ?>
+    <?php include("includes/user_profile_header.php") ?>
+    <?php include("includes/footer.php"); ?>
+
+
+
+ 
+</body>
+</html>
