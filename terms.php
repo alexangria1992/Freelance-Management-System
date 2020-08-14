30c4795c2bca11623bfdf391710f33707e37cbc0
alexangria1992 <alex040892@gmail.com>
2020-08-15 10:35:06 +1200

    created terms and conditions file page

---
+++ terms.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Computer-Fever Terms & Conditions</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <link href="styles/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/style.css"> 
+    <link rel="stylesheet" href="styles/category_nav_style.css"> 
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+
+
+</head>
+<body>
+    <?php include("includes/header.php"); ?>
+        <div class="container-fluid-mt-5 mb-5">
+            <div class="row-mb-3">
+                <div class="col-md-12 text-center">
+                    <h1>Terms & Conditions</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <ul class="nav-pills flex-column mt-2">
+                                <li class="nav-item">
+                                    
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php include("includes/footer.php"); ?>
+
+
+
+ 
+</body>
+</html>
