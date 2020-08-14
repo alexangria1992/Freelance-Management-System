f8ed4a9acebb25fc6ad6f0079dd01e74d15ca91e
alexangria1992 <alex040892@gmail.com>
2020-08-15 11:19:46 +1200

    Created contact.php file and styled the alert warning

---
+++ contact.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Computer-Fever Customer Support</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <link href="styles/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/style.css"> 
+    <link rel="stylesheet" href="styles/category_nav_style.css"> 
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link href="styles/owl.carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/owl.theme.default.css">
+
+</head>
+<body>
+    <?php include("includes/header.php"); ?>
+  
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 mt-4">
+                    <div class="alert alert-warning rounded-0 " role="alert">
+                        <p class="lead mt-1 mb-1">
+                            <strong>Warning !</strong>
+                            You do not submit a support request, without logging in to this website.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                  <div class="card">
+                    <div class="card-header text-center">
+                        <h2>Submit A Request</h2>
+                        <p class="text-muted">
+                           If you have any questions, pease feel free to contact us, Our customer service center is working for you 24/7.
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    <?php include("includes/footer.php"); ?>
+
+
+
+ 
+</body>
+</html>
