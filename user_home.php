621798a40d214606580626c595bd3ad8ed2a7c6a
alexangria1992 <alex040892@gmail.com>
2020-08-16 13:35:29 +1200

    Established featured proposal carousel

--- user_home.php
+++ user_home.php
@@ -88,8 +88,69 @@
                                     <img src="images/user_rate.blank.png" alt="" class="rating">
                                     <span class="ml-1 mr-2"></span>
                                     </span>
-                            
+                                <div class="clearfix mb-2"></div>
                             </div>
+                            <a href="proposals/proposal.php">
+                                 <hr class="m-0 p-0">
+                                    <img src="proposals/proposal_files/youtube-seo-1.jpg" alt="" class="resp-img">
+                             </a>
+                             <div class="text">
+                                 <h4>
+                                     <a href="proposals/proposal.php" class="video-img">
+                                        I Will Do Viral Youtube Seo Social Media Promotion
+                                    </a>
+                                 </h4>
+                                 <hr>
+                                 <p class="buttons clearfix">
+                                     <a href="#" data-toggle="tooltip" title="Add to Favorites" class="favorite mt-2 float-left">
+                                         <i class="fa fa-heart fa-lg"></i>
+                                     </a>
+                                     <span class="float-right">STARTING AT <strong class="price">$5</strong>span>
+                                 </p>
+                             </div>
+                             <div class="ribbon">
+                                 <div class="theribbon">Featured</div>
+                                 <div class="ribbon-background"></div>
+                             </div>
+                        </div>
+
+                        <div class="proposal-div">
+                            <div class="proposal_nav">
+                                <span class="float-left mt-2">
+                                    <strong class="ml-2 mr-1">By</strong>miss_digi_market
+                                </span>
+                                <span class="float-right mt-2">
+                                    <img src="images/user_rate_full.png" alt="" class="rating">
+                                    <img src="images/user_rate_full.png" alt="" class="rating">
+                                    <img src="images/user_rate_full.png" alt="" class="rating">
+                                    <img src="images/user_rate_full.png" alt="" class="rating">
+                                    <img src="images/user_rate.blank.png" alt="" class="rating">
+                                    <span class="ml-1 mr-2"></span>
+                                    </span>
+                                <div class="clearfix mb-2"></div>
+                            </div>
+                            <a href="proposals/proposal.php">
+                                 <hr class="m-0 p-0">
+                                    <img src="proposals/proposal_files/youtube-seo-1.jpg" alt="" class="resp-img">
+                             </a>
+                             <div class="text">
+                                 <h4>
+                                     <a href="proposals/proposal.php" class="video-img">
+                                        I Will Do Viral Youtube Seo Social Media Promotion
+                                    </a>
+                                 </h4>
+                                 <hr>
+                                 <p class="buttons clearfix">
+                                     <a href="#" data-toggle="tooltip" title="Add to Favorites" class="favorited mt-2 float-left">
+                                         <i class="fa fa-heart fa-lg"></i>
+                                     </a>
+                                     <span class="float-right">STARTING AT <strong class="price">$5</strong>span>
+                                 </p>
+                             </div>
+                             <div class="ribbon">
+                                 <div class="theribbon">Featured</div>
+                                 <div class="ribbon-background"></div>
+                             </div>
                         </div>
                     </div>
                 </div>
