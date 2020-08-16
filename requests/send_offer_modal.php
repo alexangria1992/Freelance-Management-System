279c8f176f1645b60dce6a11f25555e9e595a6de
alexangria1992 <alex040892@gmail.com>
2020-08-16 16:02:38 +1200

    Added content to modal

--- requests/send_offer_modal.php
+++ requests/send_offer_modal.php
@@ -9,7 +9,21 @@
             </div>
             <div class="modal-body p-0">
                 <div class="request-summary">
-                    
+                    <img src="user_images/brock.jpg" width="50" height="50" class="rounded-circle">
+                    <div id="request-description">
+                        <h6 class="text-primary mb-1">Script Writing</h6>
+                        <p>I Need A Programmer To Write Me A Script</p>
+                    </div>
+                </div>
+                <div class="request-proposals-list">
+                    <div class="proposal-picture">
+                        <input type="radio" name="proposal_id" class="radio-custom" id="radio-1" required>
+                        <label for="radio-1" class="radio-custom-label"></label>
+                        <img src="proposals/proposal_files/youtube-seo-1.jpg" width="50" height="50"alt="">
+                    </div>
+                    <div class="proposal-title">
+                        I Will Do Viral YouTube Seo Social Media Promotion
+                    </div>
                 </div>
             </div>
         </div>
