347a2dbb7408857f953a8e732398ca60f301d964
alexangria1992 <alex040892@gmail.com>
2020-08-16 15:57:28 +1200

    Created modal header

---
+++ requests/send_offer_modal.php
@@ -0,0 +1,23 @@
+<div id="send-offer-modal" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">
+                    Select A Proposal To Offer
+                </h5>
+                <button class="close" data-dismiss="modal"><span>&times;</span></button>
+            </div>
+            <div class="modal-body p-0">
+                <div class="request-summary">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function(){
+        $("#send-offer-modal").modal("show");
+    })
+</script>
