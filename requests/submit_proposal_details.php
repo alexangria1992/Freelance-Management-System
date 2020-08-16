7c7cfdaba48a9b9bb4bce5f96b311930938cfc60
alexangria1992 <alex040892@gmail.com>
2020-08-16 16:52:57 +1200

    Created form in submit proposal modal

--- requests/submit_proposal_details.php
+++ requests/submit_proposal_details.php
@@ -6,6 +6,25 @@
         <button class="close" data-dismiss="modal">&times;</button>
     </div>
     <div class="modal-body p-0">
-        
+        <div class="request-summary">
+            <img src="user_images/brock.jpg" width="50" height="50"alt="" class="rounded-circle">
+            <div id="request-description">
+                <h6 class="text-primary mb-1">Script Writing</h6>
+                <p>I Need a Programmer to Write Me A Script</p>
+            </div>
+        </div>
+        <form action="" id="proposal-details-form">
+            <div class="selected-proposal p-3">
+                <h5>I Will Do Viral YoTube Seo Social Media Production</h5>
+                <hr>
+                <input type="hidden" name="proposal_id" value="">
+                <input type="hidden" name="request_id" value="">
+                <div class="form-group">
+                    <label for="" class="font-weight-bold"> Description: </label>
+                        <textarea name="description" class="form-control" required></textarea>  
+                </div>
+                <hr>
+            </div>
+        </form>
     </div>
 </div>
