7a01d9c1a8e170b0ebf30af47f09fb79c5b471c3
alexangria1992 <alex040892@gmail.com>
2020-08-16 12:35:18 +1200

    Added carousel slider to user_home.php

--- user_home.php
+++ user_home.php
@@ -1,18 +1,65 @@
 <div id="myCarousel" class="carousel slide">
     <ol class="carousel-indicators">
-        <li data-target="myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="myCarousel" data-slide-to="1" ></li>
-        <li data-target="myCarousel" data-slide-to="2" ></li>
-        <li data-target="myCarousel" data-slide-to="3" ></li>       
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1" ></li>
+        <li data-target="#myCarousel" data-slide-to="2" ></li>
+        <li data-target="#myCarousel" data-slide-to="3" ></li>       
     </ol>
     <div class="carousel-inner">
         <div class="carousel-item active">
             <a href="">
                 <img src="slides_images/image-1.jpg" class="d-block w-100" alt="">
-                <div class="carouel-caption">
-                    
+                <div class="carousel-caption">
+                    <h3 class="d-lg-block d-md-block d-none">Slide Caption</h3>
+                    <p class="d-lg-block d-md-block d-none">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. I
+                    </p>
+                </div>
+            </a>
+        </div>
+
+        <div class="carousel-item">
+            <a href="">
+                <img src="slides_images/image-3.jpg" class="d-block w-100" alt="">
+                <div class="carousel-caption">
+                    <h3 class="d-lg-block d-md-block d-none">Slide Caption</h3>
+                    <p class="d-lg-block d-md-block d-none">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. I
+                    </p>
+                </div>
+            </a>
+        </div>
+
+        <div class="carousel-item">
+            <a href="">
+                <img src="slides_images/image-4.jpg" class="d-block w-100" alt="">
+                <div class="carousel-caption">
+                    <h3 class="d-lg-block d-md-block d-none">Slide Caption</h3>
+                    <p class="d-lg-block d-md-block d-none">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. I
+                    </p>
+                </div>
+            </a>
+        </div>
+
+        <div class="carousel-item">
+            <a href="">
+                <img src="slides_images/image-1.jpg" class="d-block w-100" alt="">
+                <div class="carousel-caption">
+                    <h3 class="d-lg-block d-md-block d-none">Slide Caption</h3>
+                    <p class="d-lg-block d-md-block d-none">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. I
+                    </p>
                 </div>
             </a>
         </div>
     </div>
+<a href="#myCarousel"  data-slide="prev" class="carousel-control-prev">
+    <span class="carousel-control-prev-icon"></span>
+</a>
+
+<a href="#myCarousel" data-slide="next" class="carousel-control-next">
+    <span class="carousel-control-next-icon"></span>
+</a>
+
 </div>
