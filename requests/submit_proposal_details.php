791002439df462eb9a22adbb0ddad778099ea403
alexangria1992 <alex040892@gmail.com>
2020-08-16 17:02:35 +1200

    Added more elements to form

--- requests/submit_proposal_details.php
+++ requests/submit_proposal_details.php
@@ -19,12 +19,38 @@
                 <hr>
                 <input type="hidden" name="proposal_id" value="">
                 <input type="hidden" name="request_id" value="">
+
                 <div class="form-group">
                     <label for="" class="font-weight-bold"> Description: </label>
                         <textarea name="description" class="form-control" required></textarea>  
                 </div>
                 <hr>
+
+                <div class="form-group">
+                    <label for="" class="font-weight-bold"> Delivery Time: </label>
+                    <select name="delivery_time" id="" class="form-control float-right">
+                        <option value="1 Day">1 Day</option>
+                        <option value="2 Day">2 Days</option>
+                        <option value="3 Day">3 Days</option>
+
+                    </select>
+                </div>
+                <hr>
+
+                <div class="form-group">
+                    <label for="" class="font-weight-bold"> Total Offer Amount: </label>
+                    <div class="input-group float-right">
+                        <span class="input-group-addon font-weight-bold"> $</span>
+                        <input type="number" name="amount" min="5" class="form-control" placeholder="5 Minimum"> 
+                    </div>
+                </div>
+                
             </div>
         </form>
+    </div>
+
+    <div class="modal-footer">
+        <button class="btn btn-secondary" data-dismiss="modal" data-toggle="modal" data-target="#send-offer-modal">Back</button>
+        <button type="submit" class="btn btn-success">Submit Offer</button>
     </div>
 </div>
