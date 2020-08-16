3e1abdd895c31cc03e8a76c52176170e5a28005c
alexangria1992 <alex040892@gmail.com>
2020-08-16 13:02:16 +1200

    Created user sidebar row divs

--- includes/user_home_sidebar.php
+++ includes/user_home_sidebar.php
@@ -7,3 +7,72 @@
         </a>
     </div>
 </div>
+
+<div class="card rounded-0 mb-3">
+    <div class="card-header">
+        <h5>BUY IT AGAIN</h5>
+    </div>
+    <div class="card-body">
+        <div class="row mb-3">
+            <img src="proposals/proposal_files/youtube-seo-1.jpg" alt="" class="col-lg-4 col-md-12 col-sm-4 col-4 img-fluid user-home-img-responsive">
+            <p class="col-lg-8 col-md-12 col-sm-8 col-8 user-home-title-responsvie">
+                <a href="proposals/proposal.php">
+                    I Will Do Viral Youtube Seo Social Media Promotion
+               </a>
+            </p>
+        </div>
+
+        <div class="row mb-3">
+            <img src="proposals/proposal_files/youtube-seo-1.jpg" alt="" class="col-lg-4 col-md-12 col-sm-4 col-4 img-fluid user-home-img-responsive">
+            <p class="col-lg-8 col-md-12 col-sm-8 col-8 user-home-title-responsvie">
+                <a href="proposals/proposal.php">
+                    I Will Do Viral Youtube Seo Social Media Promotion
+               </a>
+            </p>
+        </div>
+
+        <div class="row mb-3">
+            <img src="proposals/proposal_files/youtube-seo-1.jpg" alt="" class="col-lg-4 col-md-12 col-sm-4 col-4 img-fluid user-home-img-responsive">
+            <p class="col-lg-8 col-md-12 col-sm-8 col-8 user-home-title-responsvie">
+                <a href="proposals/proposal.php">
+                    I Will Do Viral Youtube Seo Social Media Promotion
+               </a>
+            </p>
+        </div>
+    </div>
+</div>
+
+
+<div class="card rounded-0 mb-3">
+    <div class="card-header">
+        <h5>RECENTY VIEWED</h5>
+    </div>
+    <div class="card-body">
+        <div class="row mb-3">
+            <img src="proposals/proposal_files/youtube-seo-1.jpg" alt="" class="col-lg-4 col-md-12 col-sm-4 col-4 img-fluid user-home-img-responsive">
+            <p class="col-lg-8 col-md-12 col-sm-8 col-8 user-home-title-responsvie">
+                <a href="proposals/proposal.php">
+                    I Will Do Viral Youtube Seo Social Media Promotion
+               </a>
+            </p>
+        </div>
+
+        <div class="row mb-3">
+            <img src="proposals/proposal_files/youtube-seo-1.jpg" alt="" class="col-lg-4 col-md-12 col-sm-4 col-4 img-fluid user-home-img-responsive">
+            <p class="col-lg-8 col-md-12 col-sm-8 col-8 user-home-title-responsvie">
+                <a href="proposals/proposal.php">
+                    I Will Do Viral Youtube Seo Social Media Promotion
+               </a>
+            </p>
+        </div>
+
+        <div class="row mb-3">
+            <img src="proposals/proposal_files/youtube-seo-1.jpg" alt="" class="col-lg-4 col-md-12 col-sm-4 col-4 img-fluid user-home-img-responsive">
+            <p class="col-lg-8 col-md-12 col-sm-8 col-8 user-home-title-responsvie">
+                <a href="proposals/proposal.php">
+                    I Will Do Viral Youtube Seo Social Media Promotion
+               </a>
+            </p>
+        </div>
+    </div>
+</div>
