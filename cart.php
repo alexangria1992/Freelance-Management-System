2a1597fc00b92309a23e77f57dce5892ee96ccd0
alexangria1992 <alex040892@gmail.com>
2020-08-20 23:12:39 +1200

    Created cart.php

--- cart.php
+++ cart.php
@@ -36,13 +36,89 @@
                     <div class="card mb-3">
                         <div class="card-body">
                             <div class="cart-proposal">
+                                <div class="row">
+                                    <div class="col-lg-3 mb-2">
+                                        <a href="proposals/proposal.php">
+                                            <img src="proposals/proposal_files/youtube-seo-1.jpg" class="img-fluid" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="col-lg-9">
+                                        <a href="proposals/proposal.php">
+                                            <h6>I Will Do Viral Youtube Seo Social Media Promotion</h6>
+                                        </a>
+                                        <a href="cart.php?remove_proposal" class="remove-link text-muted">
+                                            Remove Proposal
+                                        </a>
+                                    </div>
+                                </div>
+                                <hr>
+                                <h6 class="clearfix">
+                                    Proposal Quantity
+                                    <strong class="float-right price ml-2 mt-2">$20</strong>
+                                    <input type="text" name="quantity" class="float-right form-control quantity" data-proposal_id=""
+                                    value="1">  
+                                </h6>
+                                <hr>
+
+                            </div>
+
+                            <div class="cart-proposal">
+                                <div class="row">
+                                    <div class="col-lg-3 mb-2">
+                                        <a href="proposals/proposal.php">
+                                            <img src="proposals/proposal_files/youtube-seo-1.jpg" class="img-fluid" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="col-lg-9">
+                                        <a href="proposals/proposal.php">
+                                            <h6>I Will Do Viral Youtube Seo Social Media Promotion</h6>
+                                        </a>
+                                        <a href="cart.php?remove_proposal" class="remove-link text-muted">
+                                            Remove Proposal
+                                        </a>
+                                    </div>
+                                </div>
+                                <hr>
+                                <h6 class="clearfix">
+                                    Proposal Quantity
+                                    <strong class="float-right price ml-2 mt-2">$20</strong>
+                                    <input type="text" name="quantity" class="float-right form-control quantity" data-proposal_id=""
+                                    value="1">  
+                                </h6>
+                                <hr>
                                 
                             </div>
+
+                            <h3 class="float-right">Total: $40</h3>
                         </div>
                     </div>
                 </div>
+                    <div class="col-md-5">
+                        <div class="card">
+                            <div class="card-body cart-order-details">
+                                <p>
+                                    Cart Subtotal <span class="float-right">$40</span>
+                                </p>
+                                <hr>
+                                <p>Apply Couon Code</p>
+                                <form action="" class="input-group" method="post">
+                                    <input type="text" name="code" class="form-control apply-disabled" placeholder="Enter Coupon Code">
+                                    <button type="submit" name="coupon-submit" class="input-group-addon btn btn-success">Apply</button>
+                                </form>
+                                <p class="coupon-response"></p>
+                                <hr>
+                                <p>Processing Fee <span class="float-right">$1</span></p>
+                                <hr>
+                                <p>Total <span class="font-weight-bold float-right">$1</span></p>
+                                <hr>
+                                <a href="card_payment_options.php" class="btn btn-lg btn-success btn-block">
+                                    Proceed To Payment
+                                </a>
+                            </div>
+                        </div>
+                    </div>            
+                </div>
             </div>
-        </div>
     <?php include("includes/footer.php"); ?>
 
 
---
+++ cart_payment_options.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Computer-Fever Shopping Payment Options</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <link href="styles/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/style.css"> 
+    <link rel="stylesheet" href="styles/category_nav_style.css"> 
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link href="styles/owl.carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/owl.theme.default.css">
+    <script src="js/jquery.min.js"></script>
+</head>
+<body>
+    <?php include("includes/header.php"); ?>
+        <div class="container mt-5 mb-5">
+           <div class="row">
+               <div class="col-md-12">
+                   <div class="card mb-3">
+                       <div class="card-body">
+                           <h5 class="float-left">Your Cart (2)</h5>
+                           <h5 class="float-right">
+                               <a href="index.php">
+                                   Keep Shopping
+                               </a>
+                           </h5>
+                       </div>
+                   </div>
+               </div>
+           </div> 
+
+           <div class="row">
+               <div class="col-md-7">
+                   <div class="row">
+                       <div class="col-md-12 mb-3">
+                           <div class="card payment-options">
+                               <div class="card-header">
+                                   <h5>Available Shopping Balance</h5>
+                               </div>
+                                <div class="card-body">
+                                  <div class="row">
+                                      <div class="col-1">
+                                          <input type="radio" id="shopping-balance" name="method" class="form-control radio-input" checked>
+                                      </div>
+                                      <div class="col-11">
+                                        <p class="lead mt-2">
+                                            Personal Balance - fixmywebsite
+                                        <span class="text-success font-weight-bold">$198</span>
+                                        </p>
+                                      </div>
+                                    </div>  
+                                </div>
+                           </div>
+                       </div>
+
+                       <div class="col-md-12 md-3">
+                           <div class="card payment-options">
+                               <div class="card-header">
+                                   <h5>Payment Options</h5>
+                               </div>
+                               <div class="card-body">
+                                   <div class="row">
+                                    <div class="col-1">
+                                        <input type="radio" id="paypal" class="form-control radio-input" name="method">
+                                    </div>
+                                    <div class="col-11">
+                                        <img src="images/paypal.png" height="50" class="ml-2 width-xs-100" alt="">
+                                    </div>
+                                    </div>
+                                    <hr>
+                                    <div class="row">
+                                    <div class="col-1">
+                                        <input type="radio" id="credit-card" class="form-control radio-input" name="method">
+                                    </div>
+                                    <div class="col-11">
+                                        <img src="images/credit_cards.jpg" height="50" class="ml-2 width-xs-100" alt="">
+                                    </div>
+                                   </div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+
+               <div class="col-md-5">
+                <div class="card">
+                    <div class="card-body cart-order-details">
+                        <p>Cart Subtotal <span class="float-right">$40</span></p>
+                        <hr>
+                        <p class="processing-fee">Processing Fee <span class="float-right">$1</span></p>
+                        <hr class="processing-fee">
+                        <p>Total <span class="float-right font-weight-bold total-price">$41</span></p>
+                        <hr>
+                        <form action="shopping_balance.php" method="post" id="shopping-balance-form">
+                            <input type="hidden" name="amount" value="">
+                            <button class="btn btn-lg btn-success btn-block" type="submit" name="cart_submit_order" 
+                            onclick="return confirm('Do You Really Want To Order Proposals From Your Shopping Balance.')">
+                            Pay With Shopping balance
+                           </button>
+                        </form>
+                    </div>
+                </div>
+               </div>
+           </div>
+        </div>
+    <?php include("includes/footer.php"); ?>
+
+
+
+ 
+</body>
+</html>
