157ee51fb9d7f0cc7fb5a3ea72ef9ab28c9a90ce
alexangria1992 <alex040892@gmail.com>
2020-08-20 17:47:41 +1200

    Started cart.php

---
+++ cart.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Computer-Fever Freelance Services Marketplace</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <link href="styles/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/style.css"> 
+    <link rel="stylesheet" href="styles/category_nav_style.css"> 
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link href="styles/owl.carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/owl.theme.default.css">
+    <script src="js/jquery.min.js"></script>
+</head>
+<body>
+    <?php include("includes/header.php"); ?>
+
+        <div class="container mt-5 mb-3">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h5 class="float-left">Your Cart (2)</h5>
+                            <h5 class="float-right">
+                                <a href="index.php">
+                                    Keep Shopping
+                                </a>
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row" id="cart-show">
+                <div class="col-md-7">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <div class="cart-proposal">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php include("includes/footer.php"); ?>
+
+
+
+ 
+</body>
+</html>
