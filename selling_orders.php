fae450d87647584f50ed55fb5492cb67e363df86
alexangria1992 <alex040892@gmail.com>
2020-08-21 21:43:27 +1200

    Completed checkout markup and began selling_orders.php

---
+++ selling_orders.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Computer-Fever/ Proposals Sale Orders</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <link href="styles/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/style.css"> 
+    <link rel="stylesheet" href="styles/category_nav_style.css"> 
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+    <link href="styles/owl.carousel.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/owl.theme.default.css">
+    <script src="js/jquery.min.js"></script>
+</head>
+<body>
+    <?php include("includes/header.php"); ?>
+        <div class="container-fluid mt-5">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Manage Proposals Sales Orders</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 mt-5 mb-3">
+                    
+                </div>
+            </div>
+        </div>
+
+
+    <?php include("includes/footer.php"); ?>
+
+
+
+ 
+</body>
+</html>
